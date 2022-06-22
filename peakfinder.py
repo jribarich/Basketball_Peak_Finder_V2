5840eb2fd7bc179e92e0ba71da8267d4264a3ebb
Jack Ribarich <jackribarich@yahoo.com>
2022-06-21 22:17:02 -0700

    Removed Player class to allow for session variables

--- peakfinder.py
+++ peakfinder.py
@@ -11,57 +11,34 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-class Player():
-    def __init__(self, name = None, ext = None, 
-        nicknames = None, position = None, height = None, 
-        hand = None, college = None, reg_season = None, 
-        playoff = None, adv_data = None):
-        self.name = name
-        self.ext = ext  
-        self.nicknames = nicknames  # list
-        self.position = position
-        self.height = height
-        self.hand = hand 
-        self.college = college  
-        self.reg_season = reg_season  # panda DataFrame
-        self.playoff = playoff  # panda DataFrame
-
-    def __repr__(self):
-        return(f"""Nicknames: {self.nicknames}\nPosition: {self.position}
-                    Height: {self.height}\nShoots: {self.hand}\nCollege: {self.college}\n
-                    Regular Season Stats:\n{self.reg_season}\n
-                    Playoff Stats:\n{self.playoff}\n""")
-
-
 def create_sidebar():
     sb = st.sidebar
-    sb.title("Basketball Peak Finder V2")
+    sb.title(":basketball: Basketball Peak Finder V2")
     name = sb.text_input("Enter a player's name:", value="").strip().lower()
     option = sb.selectbox("Select an option:", ["Regular Season", "Playoffs"])
 
     return sb, name, option
 
 
-def get_player(player, name):
+def get_player(name):
     with open('player_database.txt') as f:
         player_dict = ast.literal_eval(f.read())
     
-    player.name = name
+    name = name
     
     try:
-        player.ext = player_dict[player.name]  # search players.py dict
+        ext = player_dict[name]  # search players.py dict
+        return ext
 
     except: 
             return None
 
 def display_pic(pic):
-    col1, col2, col3 = st.columns(3)
-   
     if pic == './bball_logo.png':
-        col2.image(pic, width=200)
+        st.image(pic, width=200)
 
     else:
-        col2.image(pic)
+        st.image(pic)
     
     st.markdown('---')
 
@@ -80,15 +57,16 @@ def get_pic(soup):
     
 
 
-def player_info(player, soup):
+def player_info(soup):
     position_dict = {'Point Guard': 'PG', 'Shooting Guard': 'SG', 
                     'Small Forward': 'SF', 'Power Forward': 'PF',
                     'Center': 'C'}
     height_flag = 0
     nickname_flag = 0
+    nicknames = []
 
-    st.subheader('Player Info')
-    col1, col2, col3 = st.columns(3)
+    # st.subheader('Player Info')
+    exp = st.expander('Player Info')
 
     info = soup.find('div', attrs={'id': 'meta'})
     paragraphs = info.find_all('p')
@@ -99,53 +77,51 @@ def player_info(player, soup):
             nickname_flag = 1
             
             if  height_flag == 0:
-                player.nicknames = item.text.replace('(', '').replace(')', '').split(',')
+                nicknames = ', '.join(item.text.replace('(', '').replace(')', '').split(',')[0:3]) # max of 3 names
 
 
-            col1.write("***Nickname(s):***")
+            exp.write("***Nickname(s):***")
             
-            if player.nicknames == None:
-                    col1.write("None")
+            if len(nicknames) == 0:
+                    exp.write("None")
             else:    
                 count = 0
-                for name in player.nicknames:
                 
-                    col1.write(name)
-                    count += 1
+                exp.write(nicknames)
+                count += 1
 
-                    if count == 3:
-                        break
+                if count == 3:
+                    break
 
         # get height
         if height_flag == 1:
-            col3.write('***Height:***')
-            player.height = item.text.split(",")[0]
-            col3.write(player.height)
+            exp.write('***Height:***')
+            height = item.text.split(",")[0]
+            exp.write(height)
             height_flag = 0
         
         # get positions
         if 'Position:' in item.text:
             height_flag = 1
-            col2.write("***Position(s):***")
+            position = []
+            exp.write("***Position(s):***")
             for p in position_dict.keys():
                 if p in item.text:
-                    player.position = position_dict[p]
+                    position.append(position_dict[p])
                     
-                    col2.write(player.position)
-
-    st.markdown('---')
+            exp.write(', '.join(position))
 
 
-def player_data(player, player_url):
+def player_data(player_url):
     response = requests.get(player_url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     get_pic(soup)
-    player_info(player, soup)
+    player_info(soup)
     st.session_state['soup'] = soup
 
 
-def player_tables(player, url):
+def player_tables(url):
     drop_list = ['Age','Tm', 'Lg', 'Pos', 'G', 'MP']
 
     per_game = pd.read_html(url, attrs = {"id" : "per_game"})[0]
@@ -159,7 +135,7 @@ def player_tables(player, url):
 
     # Drop duplicate columns then merge
     per_game.drop(drop_list, axis=1, inplace=True)
-    player.reg_season = pd.merge(per_game, adv, how='outer', on='Season')
+    reg_season = pd.merge(per_game, adv, how='outer', on='Season')
 
     #pl is playoffs
     try:
@@ -174,11 +150,12 @@ def player_tables(player, url):
 
         pl_per_game.drop(drop_list, axis=1, inplace=True)
 
-        player.playoff = pd.merge(pl_per_game, pl_adv, how='outer', on='Season')
+        playoff = pd.merge(pl_per_game, pl_adv, how='outer', on='Season')
+
+        return reg_season, playoff
 
     except:
-        pl_per_game = None
-        pl_adv = None
+        return reg_season, None
 
 
 def peak_calculation(df):
@@ -193,7 +170,7 @@ def peak_calculation(df):
     return calc
 
 
-def determine_peak_season(player): 
+def determine_peak_season(reg_season, playoff): 
     peak_data = [None, None]
     peak_list = None
     seasons = None
@@ -201,8 +178,8 @@ def determine_peak_season(player):
     ppg = None 
     apg = None
     rpg  = None
-    reg = player.reg_season 
-    plof = player.playoff
+    reg = reg_season 
+    plof = playoff
 
     reg['sum'] = peak_calculation(reg)
     idx = reg['sum'].idxmax()  # max season index
@@ -220,7 +197,7 @@ def determine_peak_season(player):
     
     peak_data[0] = [peak_list, seasons, seas_sum, ppg, apg, rpg]
 
-    if not isinstance(plof, type(None)):  # playoffs
+    if plof is not None:  # playoffs
         plof['sum'] = peak_calculation(plof)
         idx = plof['sum'].idxmax()
         
@@ -243,14 +220,14 @@ def graph(peak, seasons, seas_sum, ppg, apg, rpg):
     # idx = np.arange(len(peak[1]))  # turns season into a list of indices
     # peak_sum = peak[2]
     # max_idx = peak[2].index(max(peak[2]))
-    st.subheader('Peak Season Stats')
-    mid_c1, mid_c2 = st.columns(2)
-    col1, col2, col3 = st.columns(3)
+    # st.subheader('Peak Season Stats')
+    exp = st.expander('Peak Season Stats')
+    mid_c1, mid_c2 = exp.columns(2)
+    col1, col2, col3 = exp.columns(3)
     
-    mid_c1.write("***Season:***")
-    mid_c1.write(peak[0])
-    mid_c2.write("***Team:***")
-    mid_c2.write(peak[1])
+
+    mid_c1.metric('Season:', peak[0])
+    mid_c2.metric('Team', peak[1])
     col1.write("***Player Efficiency Rating:***")
     col1.write(float(peak[2]))
     col2.write("***Win Shares:***")
@@ -263,9 +240,10 @@ def graph(peak, seasons, seas_sum, ppg, apg, rpg):
     col2.write(float(peak[6]))
     col3.write("***Rebounds:***")
     col3.write(float(peak[7]))
-    st.markdown("---")
+    # st.markdown("---")
     
-    st.subheader('Peak Index')
+    # st.subheader('Peak Index Chart')
+    exp2 = st.expander('Peak Index Chart')
 
     df = pd.DataFrame(index=seasons, columns=['Peak'])
     df['Peak'] = seas_sum
@@ -277,11 +255,9 @@ def graph(peak, seasons, seas_sum, ppg, apg, rpg):
         tooltip=['index', 'Peak'],    
     )
 
-    st.altair_chart(graph1, use_container_width=True)
+    exp2.altair_chart(graph1, use_container_width=True)
 
-    st.markdown('---')
-
-    st.subheader("Per Game Stats")
+    exp3 = st.expander('Career Chart')
     
     stats = pd.DataFrame(index=seasons, columns=['PTS', 'AST', 'REB'])
     stats['PTS'] = ppg
@@ -290,7 +266,6 @@ def graph(peak, seasons, seas_sum, ppg, apg, rpg):
 
     data = stats.reset_index().melt('index')
 
-
     graph = alt.Chart(data).mark_line().encode(
     x=alt.X('index', title='Seasons', axis=alt.Axis(labelAngle=-45)),
     y=alt.Y('value', title='Stats'),
@@ -298,7 +273,7 @@ def graph(peak, seasons, seas_sum, ppg, apg, rpg):
     tooltip=['index', 'value'],    
     ).interactive()
 
-    st.altair_chart(graph, use_container_width=True)
+    exp3.altair_chart(graph, use_container_width=True)
 
 
 def display_graphs(peak_data, option):
@@ -312,7 +287,7 @@ def display_graphs(peak_data, option):
         graph(peak_list, seasons, seas_sum, ppg, apg, rpg)
     
     else:
-        if len(peak_data) == 2:
+        if peak_data[1] is not None:
             peak_list = peak_data[1][0]
             seasons = peak_data[1][1]
             seas_sum = peak_data[1][2]
@@ -320,28 +295,29 @@ def display_graphs(peak_data, option):
             apg = peak_data[1][4]
             rpg = peak_data[1][5]
             graph(peak_list, seasons, seas_sum, ppg, apg, rpg)
+        else:
+            st.subheader('Player never made playoffs :grimacing:')
     
 
-def player_stats(player, option):
+def player_stats(name, ext, option):
     url = 'https://www.basketball-reference.com/players/'
     end = '.html'
 
-    player_url = url + player.ext[0] + '/' + player.ext + end
+    player_url = url + ext[0] + '/' + ext + end
 
 
-    if player.name != st.session_state['name']:
-        player_data(player, player_url)
-        player_tables(player, player_url)
-        peak_data = determine_peak_season(player)
-
-        st.session_state['name'] = player.name
+    if name != st.session_state['name']:
+        player_data(player_url)
+        regular, playoffs = player_tables(player_url)
+        peak_data = determine_peak_season(regular, playoffs)
+        st.session_state['name'] = name
         st.session_state['peak_data'] = peak_data
         
     
     else:
         display_pic(st.session_state['pic_url'])
+        player_info(st.session_state['soup'])
         
-
     display_graphs(st.session_state['peak_data'], option)
 
 
@@ -349,17 +325,14 @@ def main():
     sb, name, option = create_sidebar()
     
     if name != '':
-        player = Player() # instantiates player object
-        player.name = name
+        ext = get_player(name) # retrieves player URL
 
-        get_player(player, name) # retrieves player URL
-
-        if player.ext == None:
+        if ext == None:
             sb.error("Player not found. Please check spelling and try again.")
 
         else:
-            st.subheader(player.name.upper())
-            player_stats(player, option)
+            st.subheader(name.upper())
+            player_stats(name, ext, option)
 
 
 if __name__ == "__main__":
@@ -373,4 +346,7 @@ if 'peak_data' not in st.session_state:
     st.session_state['peak_data'] = None
 
 if 'pic_url' not in st.session_state:
-    st.session_state['pic_url'] = None 
+    st.session_state['pic_url'] = None
+
+if 'soup' not in st.session_state:
+    st.session_state['soup'] = None
